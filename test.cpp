01404b798c657514b49c8a3cf70e1dbe72798715
hai hvt <haihvt.it@gmail.com>
2024-02-19 23:34:29 +0700

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n[10];
+    for(int i = 0; i<10; i++){
+        cin>>n[i];
+    }
+    cout<<n;
+    cout<<"Hi!";
+    for(int i = 0; i<10; i++){
+        cout<<n[i];
+    }
+    return 0;
+}
